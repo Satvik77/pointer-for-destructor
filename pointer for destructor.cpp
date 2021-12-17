a425ca61c75ab6d7f010316a3bae77f290f498f5
Satvik77 <83899207+Satvik77@users.noreply.github.com>
2021-12-17 16:03:34 +0530

    Add files via upload

---
+++ pointer for destructor.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class test
+{
+private:
+    int *age;
+public:
+    test()
+    {
+        age = new int();
+        *age = 0;
+        cout << endl << "NP-Constructor";
+    }
+    test(int x)
+    {
+        age = new int();
+        *age = x;
+        cout << endl << "P-Constructor";
+    }
+    ~test()
+    {
+        delete age;
+        cout << endl <<"Destructor";
+    }
+
+};
+int main()
+{
+    test obj1;
+    test obj2(10);
+
+    return 0;
+}
